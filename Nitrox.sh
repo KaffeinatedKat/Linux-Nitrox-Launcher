49db50f43137dda258413d7348efb8771c04510e
John Crawford <61567332+KaffeinatedKat@users.noreply.github.com>
2021-10-26 14:06:32 -0600

    change .local steam path to .steam

--- Nitrox.sh
+++ Nitrox.sh
@@ -1,12 +1,12 @@
 #Clone Subnautica's Compatdata path for Nitrox to run in
-cp -r ~/.local/share/Steam/steamapps/compatdata/264710 ~/.local/share/Steam/steamapps/compatdata/NitroxLauncher || failed=1
+cp -r ~/.steam/steam/steamapps/compatdata/264710 ~/.steam/steam/steamapps/compatdata/NitroxLauncher || failed=1
 
 if [[ $failed == 1 ]]; then
   echo -e "\e[31mPlease install Subnautica in the deafult steam directory and run it at least once\e[0m" && exit 1
 fi
 
-export STEAM_COMPAT_DATA_PATH=~/.local/share/Steam/steamapps/compatdata/NitroxLauncher
-export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.local/share/Steam
+export STEAM_COMPAT_DATA_PATH=~/.steam/steam/steamapps/compatdata/NitroxLauncher
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.steam/steam/
 launcherpath=~/.steam/steam/steamapps/compatdata/264710/pfx/drive_c/users/steamuser/AppData/Roaming/Nitrox
 
 #Create the Nitrox directory if it doesnt exist
